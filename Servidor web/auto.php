0e878bc9329ee02017f5e9092ac9a9bf5428cb62
David McMahon <jedmacmahonve@unal.edu.co>
2017-04-24 07:48:40 -0500

    Añadida verificación de variales en socket-server

--- Servidor web/auto.php
+++ Servidor web/auto.php
@@ -4,14 +4,14 @@ $sock = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
  socket_listen($sock, 5);
  $socket=socket_accept($sock);
  while(1){
- $mensaje = socket_read($socket,4,PHP_NORMAL_READ);
+ $mensaje = socket_read($socket,2048,PHP_NORMAL_READ);
  if ($mensaje==''){
 	 socket_close($socket);
 	 socket_close($sock);
 	 break;
  }else{
 	echo $mensaje;
-	if($mensaje=="1111"){
+	if(strpos($mensaje,"potencia=")!==false){
 		$x=date(H);
 		$s="Time now is=$x\r\n";
 		socket_write($socket, $s, strlen($s));
