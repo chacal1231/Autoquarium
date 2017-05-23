9078c72a71a8ff9198de6af6eda6a6416247a422
David McMahon <jedmacmahonve@unal.edu.co>
2017-05-23 07:39:29 -0500

    Añadida función que utiliza pH y Pantalla

--- firmware/AutoAquarium/soc-hw.c
+++ firmware/AutoAquarium/soc-hw.c
@@ -446,7 +446,7 @@ void SK6812RGBW_ram_w(void)
 
 
 /***************************************************************************
- * SK6812RGBW Functions
+ * C Functions
  */
 /**
  * strstr - Find the first substring in a %NUL terminated string
@@ -500,3 +500,25 @@ size_t strlen(const char *s)
     return sc - s;
 }
 
+char *itoa(int i, char b[]){
+    char const digit[] = "0123456789";
+    char *p = b;
+    if (i < 0)
+    {
+        *p++ = '-';
+        i *= -1;
+    }
+    int shifter = i;
+    do
+    { //Move to where representation ends
+        ++p;
+        shifter = shifter / 10;
+    } while (shifter);
+    *p = '\0';
+    do
+    { //Move back, inserting digits as u go
+        *--p = digit[i % 10];
+        i = i / 10;
+    } while (i);
+    return b;
+}
